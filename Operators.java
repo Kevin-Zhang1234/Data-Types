b38a463923f36092906a2566529d2ad87ccd3be1
Kevin Zhang <username7461@gmail.com>
2022-09-16 11:12:24 -0700

    Operators working

---
+++ Operators.java
@@ -0,0 +1,14 @@
+public class Operators
+{
+    public static void main(String[] args) {
+        int i = 10;
+        double f = 2.5;
+        boolean flag = true;
+        f = f+i;
+        flag = !flag;
+        i += 1;
+        System.out.println(i);
+        System.out.println(f);
+        System.out.println(flag);
+    }
+}
